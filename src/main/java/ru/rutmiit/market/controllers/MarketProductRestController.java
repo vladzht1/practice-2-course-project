fbe80bda1a59a8d9e46e84da8a32f99356a32223
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-07-15 21:44:12 +0300

    chore: add market product controller

---
+++ src/main/java/ru/rutmiit/market/controllers/MarketProductRestController.java
@@ -0,0 +1,63 @@
+package ru.rutmiit.market.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.rutmiit.market.dtos.MarketProductDto;
+import ru.rutmiit.market.dtos.api.AddMarketProductDto;
+import ru.rutmiit.market.dtos.api.UpdateMarketProductDto;
+import ru.rutmiit.market.exceptions.MarketProductNotFoundException;
+import ru.rutmiit.market.services.MarketProductService;
+
+@RestController
+@RequestMapping("/api/market_products")
+public class MarketProductRestController {
+    @Autowired
+    private MarketProductService marketProductService;
+
+    @GetMapping()
+    public Iterable<MarketProductDto> getAllMarketProducts() {
+        return marketProductService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public MarketProductDto getMarketProductById(@PathVariable() int id) throws MarketProductNotFoundException {
+        Optional<MarketProductDto> marketProductOpt = marketProductService.findById(id);
+
+        if (!marketProductOpt.isPresent()) {
+            throw new MarketProductNotFoundException(id);
+        }
+
+        return marketProductOpt.get();
+    }
+
+    @PostMapping()
+    public MarketProductDto create(@RequestBody AddMarketProductDto addMarketProductDto) {
+        return marketProductService.add(addMarketProductDto);
+    }
+
+    @PatchMapping()
+    public MarketProductDto update(@RequestBody UpdateMarketProductDto updateMarketProductDto) throws MarketProductNotFoundException {
+        Optional<MarketProductDto> marketProductOpt = marketProductService.update(updateMarketProductDto);
+
+        if (!marketProductOpt.isPresent()) {
+            throw new MarketProductNotFoundException(updateMarketProductDto.getId());
+        }
+
+        return marketProductOpt.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable int id) {
+        marketProductService.remove(id);
+    }
+}
