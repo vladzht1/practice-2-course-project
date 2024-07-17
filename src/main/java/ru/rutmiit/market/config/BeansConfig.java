8dd27fe525cfd05a69a923b1bba2292e6b7fe8c3
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-07-17 04:16:55 +0300

    chore: add beans creating for price counter and model mapper

--- src/main/java/ru/rutmiit/market/config/BeansConfig.java
+++ src/main/java/ru/rutmiit/market/config/BeansConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import ru.rutmiit.market.mappers.ModelMapperFactory;
+import ru.rutmiit.market.mappers.OrderMapper;
+import ru.rutmiit.market.services.prices.PriceCounter;
+import ru.rutmiit.market.services.prices.PriceCounterImpl;
 
 @Configuration
 public class BeansConfig {
@@ -12,5 +15,15 @@ public class BeansConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapperFactory().getMapper();
+    }
+
+    @Bean
+    public PriceCounter priceCounter() {
+        return new PriceCounterImpl();
+    }
+
+    @Bean
+    public OrderMapper orderMapper() {
+        return new OrderMapper(modelMapper(), priceCounter());
     }
 }
